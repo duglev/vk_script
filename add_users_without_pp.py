0e6892af8decce4089094d2521d596dd2202e5a7
Admin <1>
2019-11-07 13:16:19 +0300

    Скрипт полностю переработан. Проверка групп вынесена в функцию. Добавлены комментарии

--- add_users_without_pp.py
+++ add_users_without_pp.py
@@ -1,55 +1,32 @@
 # -*- coding: utf-8 -*-
 import vk_api
-from variables import *
-
-PP = open("PP.txt", "r")
-PP = PP.read()
-PP = [int(x) for x in PP.split(',')]
+from variables import NN, login, password
 
 
-def auth_handler():
-    key = input("Enter authentication code: ")
-    remember_device = True
-    return key, remember_device
+def check_pp(pp_id, pp_count):  # Данная функция ищет количество совпадений среди групп пользователя
+    pp_id = open("PP.txt", "r")  # В файле содержится список id "сомнительных" пабликов
+    pp_id = pp_id.read()
+    pp_id = [int(x) for x in pp_id.split(',')]
+    groups_response = vk.groups.get(user_id=pp_id)
+    return len(set(groups_response['items']).intersection(pp_id)) > pp_count
 
 
-def main():
-    vk_session = vk_api.VkApi(
-        login, password,
-        auth_handler=auth_handler
-    )
+# Авторизация в ВК
+vk_session = vk_api.VkApi(login, password)
+vk_session.auth()
+vk = vk_session.get_api()
+# Запрашиваем список поступивших заявок в группу
+response = vk.groups.getRequests(group_id=NN, count=200)
 
-    try:
-        vk_session.auth()
-    except vk_api.AuthError as error_msg:
-        print(error_msg)
-        return
-
-    vk = vk_session.get_api()
-    response = vk.groups.getRequests(group_id=vk_id, count=200, offset=0)
-    sum_add_users = 0
-
-    for id in response['items']:
-        response = vk.users.get(user_id=id)
-        if response[0].get('deactivated') == 'deleted' or response[0].get("deactivated") == "banned":
-            vk.groups.removeUser(group_id=vk_id, user_id=id)
-            print("Отклонено: ", id, "(Учётная запись заблокирована)")
+for user_id in response['items']:
+    user_response = vk.users.get(user_id=user_id)
+    if user_response[0].get('deactivated') is not None:  # Проверка, не заблокирован ли пользователь
+        vk.groups.removeUser(group_id=NN, user_id=user_id)
+    else:
+        if user_response[0]["is_closed"]:  # Проверка, не закрыт ли профиль пользователя
+            vk.groups.approveRequest(group_id=NN, user_id=user_id)
         else:
-            if response[0]["is_closed"]:
-                vk.groups.approveRequest(group_id=vk_id, user_id=id)
-                sum_add_users += 1
+            if check_pp(user_id, 7):  # Проверка, количества сомнительных подписок
+                vk.groups.removeUser(group_id=NN, user_id=user_id)
             else:
-                response = vk.groups.get(user_id=id)
-                response = response['items']
-                if len(set(response).intersection(PP)) < 7:
-                    vk.groups.approveRequest(group_id=vk_id, user_id=id)
-                    sum_add_users += 1
-                else:
-                    vk.groups.removeUser(group_id=vk_id, user_id=id)
-                    print("Отклонено: ", id)
-
-    print("Добавленно: ", sum_add_users, " учётных записей", sep="")
-
-
-if __name__ == '__main__':
-    main()
+                vk.groups.approveRequest(group_id=NN, user_id=user_id)
