975e6cb0b8c6840e4e2a4424234ea343ddb9a549
Admin <1>
2019-11-07 15:01:06 +0300

    Исправлена ошибка в названии переменной из-за которой функция работала некорректно.

--- add_users_without_pp.py
+++ add_users_without_pp.py
@@ -4,11 +4,11 @@ from variables import NN, login, password
 
 
 def check_pp(pp_id, pp_count):  # Данная функция ищет количество совпадений среди групп пользователя
-    pp_id = open("PP.txt", "r")  # В файле содержится список id "сомнительных" пабликов
-    pp_id = pp_id.read()
-    pp_id = [int(x) for x in pp_id.split(',')]
+    pp_list_id = open("PP.txt", "r")  # В файле содержится список id "сомнительных" пабликов
+    pp_list_id = pp_list_id.read()
+    pp_list_id = [int(x) for x in pp_list_id.split(',')]
     groups_response = vk.groups.get(user_id=pp_id)
-    return len(set(groups_response['items']).intersection(pp_id)) > pp_count
+    return len(set(groups_response['items']).intersection(pp_list_id)) > pp_count
 
 
 # Авторизация в ВК
@@ -17,7 +17,7 @@ vk_session.auth()
 vk = vk_session.get_api()
 # Запрашиваем список поступивших заявок в группу
 response = vk.groups.getRequests(group_id=NN, count=200)
-response = response['items'] # Достаём из словаря список ID
+response = response['items']  # Достаём из словаря список ID
 
 for user_id in response:
     user_response = vk.users.get(user_id=user_id)
