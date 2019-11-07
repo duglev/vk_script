5155cbe27cacfad3c40c18ce884986429bf20265
Admin <1>
2019-11-07 16:13:57 +0300

    Вынес открытие файла и его обработку из функции, для ускорения работы скрипта.

--- add_users_without_pp.py
+++ add_users_without_pp.py
@@ -2,13 +2,14 @@
 import vk_api
 from variables import NN, login, password
 
+pp_list_id = open("PP.txt", "r")  # В файле содержится список id "сомнительных" пабликов
+pp_list_id = pp_list_id.read()
+pp_list_id = [int(x) for x in pp_list_id.split(',')]
 
-def check_pp(pp_id, pp_count):  # Данная функция ищет количество совпадений среди групп пользователя
-    pp_list_id = open("PP.txt", "r")  # В файле содержится список id "сомнительных" пабликов
-    pp_list_id = pp_list_id.read()
-    pp_list_id = [int(x) for x in pp_list_id.split(',')]
-    groups_response = vk.groups.get(user_id=pp_id)
-    return len(set(groups_response['items']).intersection(pp_list_id)) > pp_count
+
+def check_pp():  # Функция ищет количество совпадений среди групп пользователя
+    groups_response = vk.groups.get(user_id=user_id)
+    return len(set(groups_response['items']).intersection(pp_list_id))
 
 
 # Авторизация в ВК
@@ -27,9 +28,9 @@ for user_id in response_items:
     user_response = vk.users.get(user_id=user_id)
     if user_response[0].get('deactivated') is not None:  # Проверка, не заблокирован ли пользователь
         vk.groups.removeUser(group_id=NN, user_id=user_id)
+        remove_user += 1
         if log_view == 2:
             print("Пользователь с id", user_id, " заблокирован. Отклонён.", sep="")
-        remove_user += 1
     else:
         if user_response[0]["is_closed"]:  # Проверка, не закрыт ли профиль пользователя
             vk.groups.approveRequest(group_id=NN, user_id=user_id)
@@ -37,11 +38,11 @@ for user_id in response_items:
             if log_view == 2:
                 print("Пользователь с id", user_id, " закрыт. Добавлен.", sep="")
         else:
-            if check_pp(user_id, 7):  # Проверка, количества сомнительных подписок
+            if check_pp() > 7:  # Проверка, количества сомнительных подписок
                 vk.groups.removeUser(group_id=NN, user_id=user_id)
                 remove_user += 1
                 if log_view == 2:
-                    print("Пользователь с id", user_id, " имеет сомнительный подписки. Отклонён.", sep="")
+                    print("Пользователь с id", user_id, " Сомнительных подписок: ", check_pp(), " Отклонён.", sep="")
             else:
                 vk.groups.approveRequest(group_id=NN, user_id=user_id)
                 approve_user += 1
@@ -49,14 +50,13 @@ for user_id in response_items:
                     print("Пользователь с id", user_id, " нормальный. Добавлен.", sep="")
 
 if log_view > 0:
-    print("Выполнение скрипта завершено!")
     if response['count'] == 0:
         print("Заявки в группу отсутствуют.")
     else:
+        print("\nВыполнение скрипта завершено!")
         if approve_user > 0:
             print("Добавлено записей: ", approve_user)
         if remove_user > 0:
             print("Отклонено записей: ", remove_user)
     if response['count'] > 200:
-        print("")
-        print("Внимание! Обработаны не все заявки. Запустите скрипт ещё раз.")
+        print("\nВнимание! Обработаны не все заявки. Запустите скрипт ещё раз.")
